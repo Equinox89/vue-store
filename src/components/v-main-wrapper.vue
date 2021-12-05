2c2956708e7372c81be1452de42e86b18f217fab
Oleg <mastercrutor89@gmail.com>
2021-12-05 20:24:46 +0200

    initial commit

---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="v-main-wrapper">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "v-main-wrapper",
+  components:{},
+  props: {},
+  data(){
+    return {
+      title: 'I am the new component'
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+  .v-main-wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    max-width: 900px;
+    margin: 0 auto;
+  }
+</style>
